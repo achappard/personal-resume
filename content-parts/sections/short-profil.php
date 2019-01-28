5b7352b70e1cc2ff16e6e6caf77217cd60a0cb05
Aurélien Chappard <aurelien.chappard@deefuse.fr>
2019-01-28 15:33:07 +0100

    modify text short profile

--- content-parts/sections/short-profil.php
+++ content-parts/sections/short-profil.php
@@ -1,6 +1,6 @@
 <section id="short-profil" class="resume-section css-animation-element css-slide-up">
 	<p class="text-center">
-		Bonjour, je suis Aurélien CHAPPARD, <strong>concepteur développeur multimédia</strong> à Lyon.
+		Bonjour, je suis Aurélien CHAPPARD, <strong>concepteur développeur multimédia freelance</strong> à Lyon.
 	</p>
 	<p class="text-center">
 		Fort de <strong>12 années d’expérience</strong> et spécialisé dans le <strong>développement front-end PHP</strong>, je peux répondre à votre problématique de développement de site internet Wordpress et plus généralement PHP.
