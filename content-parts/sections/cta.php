7a88c6f44155b4e58e7ba95ce0c9c03c60bc36b4
Aurélien Chappard <aurelien.chappard@deefuse.fr>
2019-01-31 17:03:15 +0100

    New cv 2019

--- content-parts/sections/cta.php
+++ content-parts/sections/cta.php
@@ -1,5 +1,5 @@
 <section id="cta" class="resume-section css-animation-element css-slide-up">
 	<p class="text-center">
-		<a href="cv-aurelien-chappard.pdf" target="_blank" class="btn btn-border text-uppercase">Télécharger mon CV</a>
+		<a href="cv-aurelien-chappard-2019.pdf" target="_blank" class="btn btn-border text-uppercase">Télécharger mon CV</a>
 	</p>
 </section>
