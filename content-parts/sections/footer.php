f2a53e2bb57c6c1e9cae31979b7b47072ab51784
Aurélien Chappard <aurelien.chappard@deefuse.fr>
2019-10-11 13:45:47 +0200

    add wp icon to my resume font + add link

--- content-parts/sections/footer.php
+++ content-parts/sections/footer.php
@@ -3,6 +3,7 @@
 		<li><a target="_blank" href="https://twitter.com/achappard"><span class="icon icon-twitter"></span><span class="sr-only">Mon compte Twitter : @achappard</span></a></li>
 		<li><a target="_blank" href="https://www.linkedin.com/in/aurelienchappard/"><span class="icon icon-linkedin"></span><span class="sr-only">Mon profil LinkedIn: @aurelienchappard</span></a></li>
 		<li><a target="_blank" href="https://www.deefuse.fr/"><span class="icon icon-deefuse"></span><span class="sr-only">Mon site professionnel : Deefuse</span></a></li>
+		<li><a target="_blank" href="https://www.developpeur-wordpress-lyon.com/"><span class="icon icon-wordpress"></span><span class="sr-only">Développeur Worpdress Lyon</span></a></li>
 		<li><a target="_blank" href="https://github.com/achappard/"><span class="icon icon-github"></span><span class="sr-only">Mon compte Github : @achappard</span></a></li>
 		<li><a target="_blank" href="https://www.facebook.com/achappard"><span class="icon icon-facebook"></span><span class="sr-only">Mon profil FaceBook: @achappard</span></a></li>
 	</ul>
